7f101f675c2fff908235e2b25f8484d83becdca1
entsarahmed <entesara500@gmail.com>
2025-03-07 14:18:16 +0200

    Add Form View Model

---
+++ GameZone/Controllers/GamesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameZone.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
